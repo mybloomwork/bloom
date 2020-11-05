38303dc576251177783cef7559e54a717d156f2c
mybloomwork <68631267+mybloomwork@users.noreply.github.com>
2020-11-05 21:00:41 +0100

    Update api.py

--- apps/store/api.py
+++ apps/store/api.py
@@ -119,8 +119,8 @@ def create_checkout_session(request):
             payment_method_types=['card'],
             line_items=items,
             mode='payment',
-            success_url='http://157.245.140.4:8001/cart/success/',
-            cancel_url='http://157.245.140.4:8001/cart/'
+            success_url='http://142.93.118.16:8001/cart/success/',
+            cancel_url='http://142.93.118.16:8001/cart/'
         )
         payment_intent = session.payment_intent
 
